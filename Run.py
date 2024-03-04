b3acb5887e6927a964acb1f589830ad187aed19a
sahandsorouri <sahand.sorouri@gmail.com>
2024-03-04 22:14:24 +0330

    small changes to code

--- Run.py
+++ Run.py
@@ -8,13 +8,13 @@ base_filename = "Sahand Sorouri - Product Manager"
 subprocess.run(["pdflatex", f"{base_filename}.tex"])
 
 # Step 2: Convert .pdf to .png with high resolution
-# Using 'magick' command instead of 'convert' for better compatibility with newer ImageMagick versions
 subprocess.run(["magick", "convert", "-density", "300",
                 f"{base_filename}.pdf", "-quality", "100",
-                "-flatten",  # This can help with PDFs that have transparency issues
+                "-flatten",  # Helps with PDFs that have transparency issues
                 f"{base_filename}.png"])
 
-# Step 3: Remove irrelevant files
+
+# Step 4: Remove irrelevant files
 for extension in ['.aux', '.log', '.out']:
     try:
         os.remove(f"{base_filename}{extension}")
@@ -22,4 +22,4 @@ for extension in ['.aux', '.log', '.out']:
         # If the file doesn't exist, just skip it
         pass
 
-print("Done")
+print("Conversion to PDF, PNG, and HTML completed.")
